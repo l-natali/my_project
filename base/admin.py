264ecd9a9e31df0b81d2530538159ccc8b407d06
l-natali <lukienko.natasha@gmail.com>
2022-08-04 11:54:23 +0300

    models

--- base/admin.py
+++ base/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Category, Dishes, Events, Gallery
+from .models import Category, Dishes, Events, Gallery, Specials
 
 
 admin.site.register(Category)
 admin.site.register(Dishes)
 admin.site.register(Events)
 admin.site.register(Gallery)
+admin.site.register(Specials)
